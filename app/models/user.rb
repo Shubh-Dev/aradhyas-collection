5105a74eb48d52e24888539268330fb48b7c572e
Shubh <shubh.scb@gmail.com>
2023-01-31 21:36:40 +0530

    LINTERS ERR FIX

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    validates :first_name, presence: true, length: { minimum: 3, maximum: 20}
-    validates :last_name, presence: true, length: { minimum: 3, maximum: 20}
-    validates :email, presence: true
-    validates :address, presence: true
-    validates :mobile, presence: true
+  validates :first_name, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :last_name, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :email, presence: true
+  validates :address, presence: true
+  validates :mobile, presence: true
 end
