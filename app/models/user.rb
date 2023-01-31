98876e4b3f0fa85c2b8e3e4090f1bbc5165c1dd2
Shubh <shubh.scb@gmail.com>
2023-01-31 21:06:40 +0530

    USER VALIDATION ADD

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :first_name, presence: true, length: { minimum: 3, maximum: 20}
+    validates :last_name, presence: true, length: { minimum: 3, maximum: 20}
+    validates :email, presence: true
+    validates :address, presence: true
+    validates :mobile, presence: true
 end
